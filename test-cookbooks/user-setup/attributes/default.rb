7bd2dbe3b1d20538a78b4c694fd77751d8e0bb9f
“chihiro.ebara” <chihiro.ebara@net-marketing.co.jp>
2015-10-08 16:37:54 +0900

    変数に3user, 2group格納して成功

--- test-cookbooks/user-setup/attributes/default.rb
+++ test-cookbooks/user-setup/attributes/default.rb
@@ -1,3 +1,18 @@
 default["user1"]["name"] = "ebara"
 default["user1"]["uid"] = "2000"
 default["user1"]["gid"] = "2000"
+
+default["user2"]["name"] = "hisamatsu"
+default["user2"]["uid"] = "2001"
+default["user2"]["gid"] = "2000"
+
+default["user3"]["name"] = "hbadmin"
+default["user3"]["uid"] = "3000"
+default["user3"]["gid"] = "3000"
+
+default["group1"]["name"] = "netmarketing"
+default["group1"]["gid"] = "2000"
+
+
+default["group2"]["name"] = "heartbeats"
+default["group2"]["gid"] = "3000"
--- test-cookbooks/user-setup/recipes/default.rb
+++ test-cookbooks/user-setup/recipes/default.rb
@@ -6,16 +6,39 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-group  "netmarketing" do
-  gid 2000
+group  "#{node[:group1][:name]}" do
+  gid node[:group1][:gid]
   action :create
 end
+group  "#{node[:group2][:name]}" do
+  gid node[:group2][:gid]
+  action :create
+end
+
 
 user "#{node[:user1][:name]}" do
   home "/home/#{node[:user1][:name]}"
   shell "/bin/bash"
   uid node[:user1][:uid]
   gid node[:user1][:gid]
+  supports :manage_home => true
+  action :create
+end
+
+user "#{node[:user2][:name]}" do
+  home "/home/#{node[:user2][:name]}"
+  shell "/bin/bash"
+  uid node[:user2][:uid]
+  gid node[:user2][:gid]
+  supports :manage_home => true
+  action :create
+end
+
+user "#{node[:user3][:name]}" do
+  home "/home/#{node[:user3][:name]}"
+  shell "/bin/bash"
+  uid node[:user3][:uid]
+  gid node[:user3][:gid]
   supports :manage_home => true
   action :create
 end
